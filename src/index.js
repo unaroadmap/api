158da3437fba5488bc57fe98dfded1c46c5ac9b9
Wannisson Freitas Pinho <wannissonpinho@fgfcontadores.com.br>
2020-04-25 16:16:43 -0300

    Inclusao do Cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
