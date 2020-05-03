224fbbc2a204e3cf548ca702b620338d0af2c61c
Wannisson Freitas Pinho <wannissonpinho@fgfcontadores.com.br>
2020-05-03 19:12:11 -0300

    api Address

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const State = require('../models/State');
 const City = require('../models/City');
 const District = require('../models/District');
+const Address = require('../models/Address');
 //const Address = require('../models/Address');
 //const Tech = require('../models/Tech');
 
@@ -14,14 +15,14 @@ User.init(connection);
 State.init(connection);
 City.init(connection);
 District.init(connection);
-//Address.init(connection);
+Address.init(connection);
 //Tech.init(connection);
 
 User.associate(connection.models);
 State.associate(connection.models);
 City.associate(connection.models);
 District.associate(connection.models);
-//Address.associate(connection.models);
+Address.associate(connection.models);
 //Tech.associate(connection.models);
 
 module.exports = connection;
